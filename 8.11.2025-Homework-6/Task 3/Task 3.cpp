97ef3e5e00a7c4483b5e73e5f5f52a6f64b471d1
Kranov Egor <rabofsystem228@gmail.com>
2025-11-07 02:56:11 +0300

    Добавил ДЗ на 8.11.2025

---
+++ 8.11.2025-Homework-6/Task 3/Task 3.cpp
@@ -0,0 +1,24 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char** argv)
+{
+	int n = 0;
+	scanf("%d", &n);
+	int count = 0;
+	int* arr = (int*)malloc(n * sizeof(int));
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > 0)
+		{
+			count++;
+		}
+	}
+	printf("%d", count);
+	free(arr);
+	return 0;
+}
